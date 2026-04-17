f0568a70edb4c1688a2b69fdc2bc80b81fd4e545
rlin1202 <143444645+rlin1202@users.noreply.github.com>
2026-04-16 20:32:01 -0400

    richie dijkstras modifications

--- MPJava4/src/ub/cse/algo/Traversals.java
+++ MPJava4/src/ub/cse/algo/Traversals.java
@@ -92,7 +92,29 @@ class Traversals {
         distance[graph.contentProvider] = 0;
 
         PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>(new PairComparator());
+        Set<Integer> visited = new HashSet<>();
         pq.offer(new Pair<>(0,graph.contentProvider));
+
+        while(!pq.isEmpty()){
+            Pair<Integer,Integer> currentNode = pq.poll();
+            if(visited.contains(currentNode.getSecond())){
+                continue;
+            } else {
+                visited.add(currentNode.getSecond());
+
+                for(Integer neighbor : graph.get(currentNode.getSecond())){
+                    int newDistance = currentNode.getFirst() + bandwidths.get(neighbor);
+
+                    if(newDistance < distance[neighbor]){
+                        distance[neighbor] = newDistance;
+                        previous[neighbor] = currentNode.getSecond();
+                        //priorityQueue.offer(...); <--- WORK ON THIS PART
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
 
     }
